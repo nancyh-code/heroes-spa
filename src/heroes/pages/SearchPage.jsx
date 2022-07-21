578c4ecb87071c5bdde29886abb74d1050d16d7f
Nancy Hernandez <hernandezyanezhotmail.com>
2022-07-21 00:10:30 -0400

    uso de hook useForm

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import { HeroCard } from '../components/HeroCard';
+import { useForm } from '../../hooks/UseForm';
 
 export const SearchPage = () => {
+  const { searchText, name, lastName, onInputChange } = useForm({
+    searchText: '',
+    name: '',
+    lastName: '',
+  });
+
+  const onSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(searchText, name, lastName);
+  };
+
   return (
     <>
       <h1>Search</h1>
@@ -10,13 +22,33 @@ export const SearchPage = () => {
         <div className='col-5'>
           <h4>Searching</h4>
           <hr />
-          <form>
+          <form onSubmit={onSearchSubmit}>
             <input
               type='text'
               placeholder='Search a hero'
               className='form-control'
               name='searchText'
               autoComplete='off'
+              value={searchText}
+              onChange={onInputChange}
+            />
+            <input
+              type='text'
+              placeholder='Name'
+              className='form-control'
+              name='name'
+              autoComplete='off'
+              value={name}
+              onChange={onInputChange}
+            />
+            <input
+              type='text'
+              placeholder='LastName'
+              className='form-control'
+              name='lastName'
+              autoComplete='off'
+              value={lastName}
+              onChange={onInputChange}
             />
             <button className='btn btn-outline-primary mt-3'>Search</button>
           </form>
